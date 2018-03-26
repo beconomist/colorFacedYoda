c2f8e5c6ba356c67e0dc3035a10d6fbb721fd4b5
陳保文 <beconomist@gmail.com>
2018-03-26 21:16:29 +0800

    Initial Commit

---
+++ lesson0326Tests/lesson0326Tests.swift
@@ -0,0 +1,36 @@
+//
+//  lesson0326Tests.swift
+//  lesson0326Tests
+//
+//  Created by Bao on 26/03/2018.
+//  Copyright © 2018 Bao. All rights reserved.
+//
+
+import XCTest
+@testable import lesson0326
+
+class lesson0326Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
